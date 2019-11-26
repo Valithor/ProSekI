8f8a42bee5b03cc2b0511e7f36d64d799f2da0ba
Valithor <valithor9@gmail.com>
2019-11-26 23:15:42 +0100

    proba mig

--- biblioteka/biblio/models.py
+++ biblioteka/biblio/models.py
@@ -1,20 +1,77 @@
 from django.db import models
 
+class gatunek(models.Model):
+    idGatunek = models.AutoField(primary_key=True)
+    gatunekWybor = [
+        ('DRAMAT', 'dramat'),
+        ('THRILLER', 'thriller'),
+        ('DOKUMENT', 'dokument'),
+        ('AKCJA', 'akcja'),
+        ('HORROR', 'horror'),
+        ('SCIENCE FICTION', 'science fiction'),
+        ('ANIMOWANY', 'animowany'),
+    ]
+    nazwa = models.CharField(max_length=30, choices=gatunekWybor, default='DRAMAT')
 
-class ksiazka(models.Model):
-    tytul = models.CharField(max_length=200)
-autor = models.CharField(max_length=200)
-data_wydania = models.DateField()
-dzial = models.CharField(max_length=200)
+class rezyser(models.Model):
+    idRezyser = models.AutoField(primary_key=True)
+    imie = models.CharField(max_length=25, null=False)
+    nazwisko = models.CharField(max_length=25, null=False)
+    pochodzenieWybor = [
+        ('POLSKA', 'Polska'),
+        ('FRANCJA', 'Francja'),
+        ('USA', 'Usa'),
+        ('NIEMCY', 'Niemcy'),
+        ('WIELKA BRYTANIA', 'Wielka Brytania'),
+        ('DANIA', 'Dania'),
+        ('AZJA', 'Azja'),
+        ('INNE', 'Inne'),
+    ]
+    pochodzenie = models.CharField(max_length=25, choices=pochodzenieWybor, default='POLSKA')
+    dataUrodzenia = models.DateField()
 
-class uzytkownik(models.Model):
-    imie = models.CharField(max_length=200)
-    nazwisko = models.CharField(max_length=200)
-    email = models.CharField(max_length=200)
+class aktor(models.Model):
+    idAktor = models.AutoField(primary_key=True)
+    imie = models.CharField(max_length=25, null=False)
+    nazwisko = models.CharField(max_length=25, null=False)
+    pochodzenieWybor = [
+    ('POLSKA', 'Polska'),
+    ('FRANCJA', 'Francja'),
+    ('USA', 'Usa'),
+    ('NIEMCY', 'Niemcy'),
+    ('WIELKA BRYTANIA', 'Wielka Brytania'),
+    ('DANIA', 'Dania'),
+    ('AZJA', 'Azja'),
+    ('INNE', 'Inne'),
+    ]
+    pochodzenie = models.CharField(max_length=25, choices=pochodzenieWybor, default='POLSKA')
+    dataUrodzenia = models.DateField()
 
-class terminy(models.Model):
-    ksiazka = models.ForeignKey(ksiazka, on_delete=models.CASCADE)
-data_dobioru = models.DateField()
-data_oddania = models.DateField()
-uzytkownik = models.ForeignKey(uzytkownik, on_delete=models.CASCADE)
+class film(models.Model):
+    idFilm = models.AutoField(primary_key=True)
+    idRezyser = models.ForeignKey(rezyser)
+    idGatunku = models.ForeignKey(gatunek)
+    tytul = models.CharField(max_length=25, null=False)
+    dataWydania = models.DateField()
+    ocenaWybor = [
+        ('JEDEN', '1'),
+        ('DWA', '2'),
+        ('TRZY', '3'),
+        ('CZWARTY', '4'),
+        ('PIEC', '5'),
+        ('CZESC', '6'),
+        ('SIEDEM', '7'),
+        ('OSIEM', '8'),
+        ('DZIEWIEC', '9'),
+        ('DZIESIEC', '10'),
+        ]
+    ocena = models.CharField(max_length=20, choices=ocenaWybor, default='JEDEN')
+    fabula = models.CharField(max_length=200)
 
+class filmaktor(models.Model):
+    idFilm = models.ForeignKey(film)
+    idAktor = models.ForeignKey(aktor)
+
+class filmgatunek(models.Model):
+    idFilm = models.ForeignKey(film)
+    idGatunek = models.ForeignKey(gatunek)
